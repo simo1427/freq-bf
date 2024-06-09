2a8182ae08b6a57417273cd06e8521e24a8e8fcc
Simeon Atanasov <S.A.Atanasov@student.tudelft.nl>
2024-06-09 22:12:01 +0200

    Added preliminary args parsing
    Copied over from the Honours repo

--- main.cu
+++ main.cu
@@ -10,12 +10,66 @@
 #include "refBilateral.h"
 //#include <assert.h>
 
+#define PSNR_OUT
+
 #define DEBUG_OUT(x) std::cout << #x << "= " << x << "\n"
 
 int main(int argc, char** argv) {
 
     CLI::App app{"Runner for the CUDA implementation of the fast bilateral filter using Fourier series"};
 
+    double sigmaSpatial = 8;
+    int spatialKernelSize = static_cast<int>(round(sigmaSpatial * 1.5f) * 2 + 1);
+    DEBUG_OUT(spatialKernelSize);
+    double sigmaRange = 0.1;
+    float T = 2;
+    int numberOfCoefficients = 0;
+    std::string rangeKrnName;
+    range_krn_t rangeKrn = gaussian;
+
+    // Variables for the perf evaluation
+    double startSigmaRange = 0.1;
+    double endSigmaRange = 1.0;
+    int numOfVals = 10;
+
+    int startKernelSize = 9;
+    int endKernelSize = 11;
+
+    int numberOfRuns = 10; // excluding the first one
+
+    app.add_option("--spatial", sigmaSpatial, "The sigma of the spatial kernel");
+    app.add_option("--range", sigmaRange, "The sigma of the range kernel");
+    app.add_option("--rangeKrn", rangeKrnName, "The range kernel in use (gaussian - default, gaussianUnscaled, huber, tukey, lorentz)");
+    app.add_option("--kernelSize", spatialKernelSize, "The size of the spatial kernel; if not provided, it is computed based on the spatial sigma");
+    app.add_option("--coefs", numberOfCoefficients, "The number of coefficients to use for the filtering");
+
+    CLI::Option* colArg = app.add_flag("--colour", "Specifies whether the filtering is done on a colour image");
+    CLI::Option* boxArg = app.add_flag("--box", "Use a box spatial kernel instead of a Gaussian. The size will be computed from ");
+
+
+    CLI::Option* perfArg = app.add_flag("--perf-eval",
+                                        "Run a performance evaluation on the bilateral filter. "
+                                        "Will run different combination of params a number of times and save a CSV");
+
+    app.add_option("--perf-sigma-start", startSigmaRange,"Start value for sigmaRange")->needs(perfArg);
+    app.add_option("--perf-sigma-end", endSigmaRange, "End value for sigmaRange")->needs(perfArg);
+    app.add_option("--perf-sigma-num", numOfVals, "Number of values between the start and end of sigmaRange")->needs(perfArg);
+
+    app.add_option("--perf-kernel-start", startKernelSize, "Start value for the kernel size")->needs(perfArg);
+    app.add_option("--perf-kernel-end", endKernelSize, "End value for the kernel size")->needs(perfArg);
+    app.add_option("--perf-runs", numberOfRuns, "Number of runs per combination of sigmaRange and kernel size (does not count the first run that is discarded)")->needs(perfArg);
+
+    CLI::Option* psnrArg = app.add_flag("--psnr",
+                                        "Run an evaluation on the approximation based on PSNR"
+                                        "Will run different combination of params, measure PSNR and save a CSV")->excludes(perfArg);
+    perfArg = perfArg->excludes(psnrArg); // mutually exclude the perf runner and the psnr analysis
+
+    app.add_option("--psnr-sigma-start", startSigmaRange,"Start value for sigmaRange")->needs(psnrArg);
+    app.add_option("--psnr-sigma-end", endSigmaRange, "End value for sigmaRange")->needs(psnrArg);
+    app.add_option("--psnr-sigma-num", numOfVals, "Number of values between the start and end of sigmaRange")->needs(psnrArg);
+
+    app.add_option("--psnr-kernel-start", startKernelSize, "Start value for the kernel size")->needs(psnrArg);
+    app.add_option("--psnr-kernel-end", endKernelSize, "End value for the kernel size")->needs(psnrArg);
 
     std::string filename;
 
@@ -24,13 +78,8 @@ int main(int argc, char** argv) {
 
     CLI11_PARSE(app, argc, argv);
 
-    double sigmaSpatial = 5;
-    int spatialKernelSize = static_cast<int>(round(sigmaSpatial * 1.5f) * 2 + 1);
-    DEBUG_OUT(spatialKernelSize);
-    double sigmaRange = 0.1;
-    float T = 2;
-    int numberOfCoefficients = 10;
-    range_krn_t rangeKrn = gaussian;
+    rangeKrn = rangeKrnProvider(rangeKrnName);
+
 
     std::cout << "Params: \n";
     DEBUG_OUT(sigmaRange);
@@ -66,13 +115,7 @@ int main(int argc, char** argv) {
     kernel2D = kernel * kernel.t(); // for the CPU method, the kernel needs to be a 2D array!!!
 
     // call the BF function
-    BF_approx_gpu(intFrame, filterOut, kernel, sigmaRange, rangeKrn, 0, T);
-
-    std::cout << "Gaussian kernel:\n";
-    for (int i = 0; i < kernel.rows; i++) {
-        std::cout << kernel.ptr<float>()[i] << " ";
-    }
-    std::cout << std::endl;
+    BF_approx_gpu(intFrame, filterOut, kernel, sigmaRange, rangeKrn, numberOfCoefficients, T);
 
     cv::imwrite("./gpuOut.tif", filterOut);
 
